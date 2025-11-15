ea100e4646682d3f57a40448228eb798ca57181c
Luca Egli <luca.egli@mng.ch>
2025-11-15 13:04:55 +0100

    Simplify sql executions.

--- flask_app.py
+++ flask_app.py
@@ -8,6 +8,7 @@ import hashlib
 
 load_dotenv()
 
+# Load .env variables
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
@@ -16,14 +17,33 @@ DB_CONFIG = {
 }
 W_SECRET = os.getenv("W_SECRET")
 
+# Init flask app
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+# Init db
 pool = pooling.MySQLConnectionPool(pool_name="pool", pool_size=5, **DB_CONFIG)
-
 def get_conn():
     return pool.get_connection()
 
+# DB-Helper
+def db_execute(sql, params=None, write=False):
+    conn = get_conn()
+    try:
+        cur = conn.cursor(dictionary=not write)
+        cur.execute(sql, params or ())
+        if write:
+            conn.commit()
+            return None
+        return cur.fetchall()
+    finally:
+        try:
+            cur.close()
+        except:
+            pass
+        conn.close()
+
+# DON'T CHANGE
 def is_valid_signature(x_hub_signature, data, private_key):
     hash_algorithm, github_signature = x_hub_signature.split('=', 1)
     algorithm = hashlib.__dict__.get(hash_algorithm)
@@ -31,6 +51,7 @@ def is_valid_signature(x_hub_signature, data, private_key):
     mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
     return hmac.compare_digest(mac.hexdigest(), github_signature)
 
+# DON'T CHANGE
 @app.post('/update_server')
 def webhook():
     x_hub_signature = request.headers.get('X-Hub-Signature')
@@ -43,55 +64,21 @@ def webhook():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    # GET
     if request.method == "GET":
-        conn = get_conn()
-        try:
-            cur = conn.cursor(dictionary=True)
-            cur.execute("SELECT id, content, due FROM todos ORDER BY due")
-            todos = cur.fetchall()
-        finally:
-            try:
-                cur.close()
-            except:
-                pass
-            conn.close()
-
+        todos = db_execute("SELECT id, content, due FROM todos ORDER BY due")
         return render_template("main_page.html", todos=todos)
 
     # POST
     content = request.form["contents"]
     due = request.form["due_at"]
-    conn = get_conn()
-    try:
-        cur = conn.cursor()
-        cur.execute("INSERT INTO todos (content, due) VALUES (%s, %s)", (content, due, ))
-        conn.commit()
-    finally:
-        try:
-            cur.close()
-        except:
-            pass
-        conn.close()
-
+    db_execute("INSERT INTO todos (content, due) VALUES (%s, %s)", (content, due, ), True)
     return redirect(url_for("index"))
 
 @app.post("/complete")
 def complete():
-
-    # POST
     todo_id = request.form.get("id")
-    conn = get_conn()
-    try:
-        cur = conn.cursor()
-        cur.execute("DELETE FROM todos WHERE id=%s", (todo_id,))
-        conn.commit()
-    finally:
-        try:
-            cur.close()
-        except:
-            pass
-        conn.close()
-
+    db_execute("DELETE FROM todos WHERE id=%s", (todo_id,), True)
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
