334ed382065a5aa4836d1ab6f7b41c7316ed1639
jakie-jake123 <luca.marano@mng.ch>
2026-01-08 15:07:59 +0000

    probe alles zu flicken

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -1,11 +1,15 @@
+
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(250) NOT NULL UNIQUE,
+    password VARCHAR(250) NOT NULL
+);
+
 CREATE TABLE todos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    description VARCHAR(255),
-    due DATETIME NOT NULL,
-    is_exam BOOLEAN DEFAULT FALSE,
-    countdown_start INT DEFAULT NULL,
-    silent_mode BOOLEAN DEFAULT FALSE,
+    content VARCHAR(100),
+    due DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
